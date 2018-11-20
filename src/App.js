ae7cebc6ae9e057fca1ff32c9b6c5b1f5c575912
Benjamin Reiter <ben@wake.io>
2018-11-20 11:30:19 -0500

    started tile

--- src/App.js
+++ src/App.js
@@ -2,6 +2,10 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+function tile(props){
+    return <img src={props.src} alt={props.alt} className="tile" />; // TODO need to encode id in here as href
+}
+
 class App extends Component {
 	constructor(props) {
 		super(props);
@@ -13,22 +17,22 @@ class App extends Component {
 	}
   componentDidMount() {
     fetch("https://collectionapi.metmuseum.org/public/collection/v1/objects/204503")
-			.then(res => res.json())
-				.then(
-					(result) => {
-						console.log(result);
-						this.setState({
-							isLoaded: true,
-							item: result
-						});
-					},
-					(error) => {
-						this.setState({
-							isLoaded: true,
-							error
-						});
-					}
-				)
+        .then(res => res.json())
+            .then(
+                (result) => {
+                    console.log(result);
+                    this.setState({
+                            isLoaded: true,
+                            item: result
+                    });
+                },
+                (error) => {
+                    this.setState({
+                            isLoaded: true,
+                            error
+                    });
+                }
+            )
   }
   render() {
     return (
@@ -37,7 +41,7 @@ class App extends Component {
           <img src={this.state.item.primaryImage} className="App-logo" alt="primaryImage" />
 					<h1 className="Item-title">{this.state.item.title}</h1>
 					<h2 className="Item-artist">{this.state.item.artistDisplayName}</h2>
-					<h2 className="Item-medium">{this.state.item.medium}</h2>
+                                        <h3 className="Item-medium">{this.state.item.medium}</h3>
         </header>
       </div>
     );
