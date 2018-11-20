6a05f46af515bf3343221e15ec285d5ff3a3b860
Alicia Lew <alicialew@Alicias-MacBook-Pro.local>
2018-11-20 11:55:18 -0800

    fixing up the css

--- src/App.js
+++ src/App.js
@@ -38,10 +38,14 @@ class App extends Component {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={this.state.item.primaryImage} className="App-logo" alt="primaryImage" />
+          <img src={this.state.item.primaryImage} className="Primary-image" alt="primaryImage" />
 					<h1 className="Item-title">{this.state.item.title}</h1>
+          <p className="item-year"> 1994
+          </p>
 					<h2 className="Item-artist">{this.state.item.artistDisplayName}</h2>
                                         <h3 className="Item-medium">{this.state.item.medium}</h3>
+          <p className="Item-description">Bourgeois, who grew up in a family of five and built her own family of five as an adult, constructed this quintet of spiders from salvaged metal parts and stock rods of steel. The size and materials lend the work of an industrial, futuristic feel. The artist often emphasized the contrast between the sculptural value of spiders’ bodies and the spindly lengths of their legs; she likened these limbs to drawn lines, and drawing itself to spinning a web: “What is a drawing? It is a secretion, like the thread in a spider’s web… It is knitting, a spiral, a spider web and other significant organizations of space.”
+          </p>
         </header>
       </div>
     );
