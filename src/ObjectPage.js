7a8daa95b19447036eea756b80dc1fcb684ae5f5
fakenmake <alicialew@Alicias-MacBook-Pro-2.local>
2018-11-26 13:11:44 -0800

    odified the positioning of the fab so that it doesnt intersect with the other buttons

--- src/ObjectPage.js
+++ src/ObjectPage.js
@@ -6,6 +6,7 @@ import Modal from 'react-modal';
 import Met from './img/Met-logo.png';
 import Scan from './img/scan.png';
 
+
 const customStyles = {
   content : {
     top                   : '50%',
@@ -111,17 +112,17 @@ class ObjectPage extends Component {
       <div className="App">
         <header className="App-header">
         <div className="Navbar">
-            <div className="Highlights-button"><a><img src={Met} width="40px" alt="Met-logo"></img></a><a className="Profile-img" width="50px"></a></div>
+            <div className="Highlights-button" onClick={this.Highlights}><a href="#"><img src={Met} width="40px" alt="Met-logo"></img></a><a className="Profile-img" width="50px"></a></div>
             <div className="App-title">Virtual Docent</div>
+            <div className="Signed-in" onClick=""><a className="Profile-img" width="50px"></a></div>
         </div>
 
         <div className="Grid-container">
           <img src={this.state.item.primary_image} className="Primary-image" alt="primaryImage" />
 					<h1 className="Item-title">{this.getPropForLanguage('title')}</h1>
           <a className="Item-year" href = "#">{this.state.item.obj_date}</a>
-          <div className="Button-bar">
-             <a href = "#"><img src={listen} className="Button-listen" width="40px" alt="listen" /></a>
-             <a href = "#" ><img src={save} onClick={this.openModal} className="Button-save" width ="40px" alt="save"/><div className="Save-modal"></div></a></div>
+          <a className="Button-listen" href = "#"><img src={listen} width="40px" alt="listen" /></a>
+          <a className="Button-save" href = "#" ><img src={save} onClick={this.openModal}  width ="40px" alt="save"/><div className="Save-modal"></div></a>
           <p className="Item-medium">{this.getPropForLanguage('medium')}</p>
           <div className="skinny-break"></div>
           <p className="Item-artist">{this.state.item.artist}</p>
