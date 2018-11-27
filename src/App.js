77d57b9557fa3be45b0a861d3a51d1d490677c3c
fakenmake <alicialew@Alicias-MacBook-Pro-2.local>
2018-11-27 14:51:26 -0800

    added the grid and initial assets for the museums in the My Saves view.

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 import Highlights from './Highlights.js';
 import ObjectPage from './ObjectPage.js';
 import {Welcome, CameraPage} from './Welcome.js';
+import MySaves from './MySaves.js';
 
 const AppRouter = () => (
   <Router>
@@ -12,6 +13,7 @@ const AppRouter = () => (
       <Route path="/camera" exact component={CameraPage} />
       <Route path="/highlights/" component={Highlights} />
       <Route path="/object/:id" component={ObjectPage} />
+      <Route path="/mysaves" component={MySaves} />
 	</div>
   </Router>
 );
--- src/MySaves.js
+++ src/MySaves.js
@@ -7,14 +7,23 @@ class MySaves extends Component {
   render() {
     return (
       <div className ="View-container">
-
         <div className="My-saves">
               <div className="back">close</div>
               <div className="My-saves-title-text">My Gallery</div>
               <div className="My-saves-text">Your saved works live here</div>
-              <div className="Museum">
-                <div className="Saved-work"><a href="#"><image src="" alt="work-you-saved"></image></a></div>
-              </div>
+          <div className="Museum" id="met">
+            <div className="Museum-header">
+                <div className="Museum-title">the Met Museum</div>
+                <div className="Date-visited">Jan 2019</div>
+                <div className="Museum-city">New York, NY</div>
+            </div>
+            <div className="Staggered-grid">
+                <div className="Item-a"></div>
+                <div className="Item-b"></div>
+                <div className="Item-b"></div>
+                <div className="Item-a"></div>
+            </div>
+          </div>
         </div>
       </div>
     );
