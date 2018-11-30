77de6b9b6931c55ed32208c7369cc81f2429dfd4
fakenmake <alicialew@Alicias-MacBook-Pro-2.local>
2018-11-29 16:25:11 -0800

    added instruction text for the fab

--- src/fab.js
+++ src/fab.js
@@ -7,11 +7,19 @@ import {Link} from "react-router-dom";
 class Fab extends Component {
 render() {
   return (
-
+<div className="Fixed-area">
+    <div className="Fab-instruction">
+      <div className="Fab-text">You can scan more work right here 👉</div>
+      <div className="Button-okay"><Link to ="#">Got it!</Link></div>
+    </div>
 
       <div className="Fab">
       <Link to="/camera" className="Button-scan"><a href="#"><img src={Scan} width="80px"></img></a></Link>
       </div>
+
+      </div>
+
+
   );
   }
 }
