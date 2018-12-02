e8716dbf75a8457995153aa36bcc5b99779b5b26
Benjamin Reiter <ben@wake.io>
2018-12-02 00:19:44 -0500

    improved fab hide

--- src/fab.js
+++ src/fab.js
@@ -5,23 +5,34 @@ import {Link} from "react-router-dom";
 
 
 class Fab extends Component {
-render() {
-  return (
-<div className="Fixed-area">
-    <div className="Fab-instruction">
-      <div className="Fab-text">You can scan more work right here 👉</div>
-      <div className="Button-okay"><Link to ="#">Got it!</Link></div>
-    </div>
+    constructor(props) {
+        super(props);
+        this.state = {showInstruction:true};
+        this.hideInstruction = this.hideInstruction.bind(this);
+    }
+    hideInstruction(){
+        this.setState({showInstruction: false});
+    }
+    render() {
+        var instruction = null;
+        if (this.state.showInstruction){
+            instruction = <div className="Fab-instruction">
+                          <div className="Fab-text">You can scan more work right here 👉</div>
+                          <div className="Button-okay"><a onClick={this.hideInstruction}>Got it!</a></div>
+                               </div>;
+        }
 
-      <div className="Fab">
-      <Link to="/camera" className="Button-scan"><a href="#"><img src={Scan} width="80px"></img></a></Link>
-      </div>
+      return (
+          <div className="Fixed-area">
+              <div className="Fab">
+                  {instruction}
+                  <Link to="/camera" className="Button-scan"><a href="#"><img src={Scan} width="80px"></img></a></Link>
+              </div>
+          </div>
 
-      </div>
 
-
-  );
-  }
+      );
+      }
 }
 
 export default Fab;
