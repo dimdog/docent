4e60e3114ab92a91673bbced8421ba7f82633a7c
Benjamin Reiter <ben@wake.io>
2018-12-02 00:01:27 -0500

    fixed camera

--- src/Welcome.js
+++ src/Welcome.js
@@ -61,7 +61,7 @@ class Camera extends Component {
 
             <div className="Camera-view">
             <div className="Intro-text">Allow camera access to start scanning</div>
-            <div className="Allow-access-button"><Link to=""  href="#" onClick={this.activateCamera}> Allow Access </Link></div>
+            <div className="Allow-access-button"><a  href="#" onClick={this.activateCamera}> Allow Access </a></div>
             </div>
 
           ];
