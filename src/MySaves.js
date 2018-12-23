454a88a36b7dc7e41a376326312ff9ad33dc3344
Benjamin Reiter <ben@wake.io>
2018-12-23 18:01:03 -0500

    testing mysaves

--- src/MySaves.js
+++ src/MySaves.js
@@ -1,9 +1,5 @@
 import React, { Component } from 'react';
-import Met from './img/met-shadow.png';
 import {Link} from "react-router-dom";
-import MetBkg from './img/met-bkg.png';
-import Rijks from './img/rijksmuseum-logo.png';
-import RijksBkg from './img/rijks-bkg.png';
 import UserGallery from './UserGallery.js';
 
 
@@ -34,28 +30,7 @@ class MySaves extends Component {
                   <div className="My-saves-title-text">My Gallery</div>
                   <div className="My-saves-text">Your saved works live here</div>
             </div>
-            <div className="Museum view" id="met">
-                <div className="Museum-header" height="250px">
-                    <div className="Met-bkg"><img src={MetBkg} width ="100%"></img></div>
-                    <div className="Details-overlay">
-                      <div className="Museum-title"> <img src={Met} width="150px" alt="the Met Museum"></img></div>
-                      <div className="Date-visited">Jan 2019</div>
-                      <div className="Museum-city">New York, NY</div>
-                    </div>
-                </div>
-              </div>
-            <div className="Museum view" id="rijks">
-                  <div className="Museum-header">
-                      <div className="Rijks-bkg"><img src={RijksBkg} width ="100%" height="250px"></img></div>
-                        <div className="Details-overlay">
-                          <div className="Museum-title"> <img src={Rijks} width="250px" alt="the Rijks Museum"></img></div>
-                          <div className="Date-visited">March 2019</div>
-                          <div className="Museum-city">Amsterdam, NL</div>
-                        </div>
-                      </div>
-
-                  <UserGallery history={this.props.history} className="view" />
-                  </div>
+            <UserGallery history={this.props.history} className="view" />
             </div>
     );
   }
