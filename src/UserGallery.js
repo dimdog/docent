a3ab589dee9f702b06985ff266611eedc96b68ab
Ben Reiter <dimdog@gmail.com>
2018-12-23 19:19:56 -0500

    Mysaves update (#4)
    * testing mysaves
    
    * bug fixed

--- src/UserGallery.js
+++ src/UserGallery.js
@@ -1,6 +1,17 @@
 import React, { Component } from 'react';
 import Gallery from 'react-grid-gallery';
+import CalAcademy from './img/Header_CalAcademy.png';
+import GlassHouse from './img/Header_GlassHouse.png';
+import Met from './img/Header_Met.png';
+import NEWSM from './img/Header_NEWSM.png';
+import RISDMuseum from './img/Header_RISDMuseum.png';
+import Rijksmuseum from './img/Header_Rijksmuseum.png';
 
+var repository_map = {
+    1: Met,
+    2: Rijksmuseum,
+    3: CalAcademy
+};
 function galItemfromItem(item){
     return {
         src: item.primary_image,
@@ -10,6 +21,19 @@ function galItemfromItem(item){
         caption: item.title,
         id: item.id
     }
+}
+function templateRow(repository, tileclick){
+    var items = [];
+    repository.items.forEach(function(item){
+        items.push(galItemfromItem(item));
+    });
+    return (
+        <div className="Museum view" id={repository.name}>
+          <div className="Museum-header" height="250px">
+            <div className="gallery-header"><img src={repository_map[repository.id]} width ="100%"></img></div>
+                  <Gallery images={items} className="Gallery" onClickThumbnail={tileclick} />
+          </div>
+        </div>);
 }
 class UserGallery extends Component {
     tileClick (index){
@@ -23,19 +47,21 @@ class UserGallery extends Component {
                     items: []
             }
             this.tileClick = this.tileClick.bind(this);
+            this.content = <div />
     }
     componentDidMount() {
+        var that = this;
         fetch("https://docentapp.com/api/gallery")
             .then(res => res.json())
                 .then(
                     (result) => {
-                        var items = [];
-                        result.items.forEach(function(item){
-                            items.push(galItemfromItem(item));
+                        var rows = [];
+                        result.repositories.forEach(function(repository){
+                            rows.push(templateRow(repository, that.tileClick));
                         });
+                        this.content = rows;
                         this.setState({
                                 isLoaded: true,
-                                items: items,
                                 user: result.user
                         });
                     },
@@ -48,7 +74,7 @@ class UserGallery extends Component {
         )
     }
     render() {
-        return <Gallery images={this.state.items} className="Gallery" onClickThumbnail={this.tileClick} />
+        return <div>{this.content}</div>;
     }
 }
 export default UserGallery;
